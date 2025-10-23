a71f8d4e0e504b748289eefd9ae40781c6c43680
Ester Trevisan <ester.trevisan@academico.ifpb.edu.br>
2025-10-23 13:56:21 -0300

    :sparkles: feat: implementa solução para as questões de lista

--- ListaKotlin/src/main/kotlin/listas.kt
+++ ListaKotlin/src/main/kotlin/listas.kt
@@ -1,4 +1,25 @@
 package org.example
 
-class listas {
+fun <T> ultimo(list: List<T>): T = list.last()
+fun <T> penultimo(list: List<T>): T = list[list.size - 2]
+fun <T> isPalindrome(list: List<T>): Boolean = list == list.reversed()
+
+fun codificaLista(chars: List<Char>): List<Pair<Int, Char>> {
+    val result = mutableListOf<Pair<Int, Char>>()
+    var count = 1
+    for (i in 1 until chars.size) {
+        if (chars[i] == chars[i - 1]) count++
+        else {
+            result.add(Pair(count, chars[i - 1]))
+            count = 1
+        }
+    }
+    result.add(Pair(count, chars.last()))
+    return result
+}
+
+fun decodifica(pairs: List<Pair<Int, Char>>): List<Char> {
+    val result = mutableListOf<Char>()
+    for ((count, char) in pairs) repeat(count) { result.add(char) }
+    return result
 }
