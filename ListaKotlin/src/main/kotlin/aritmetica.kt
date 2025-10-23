bf33d1a5cf558ef8d7fcce98cf8c6c413a05ac64
Ester Trevisan <ester.trevisan@academico.ifpb.edu.br>
2025-10-23 13:56:55 -0300

    :sparkles: feat: implementa solução para as questões de aritmética

--- ListaKotlin/src/main/kotlin/aritmetica.kt
+++ ListaKotlin/src/main/kotlin/aritmetica.kt
@@ -1,4 +1,10 @@
 package org.example
 
-class aritmetica {
+fun Int.isPrimo(): Boolean {
+    if (this < 2) return false
+    for (i in 2 until this) if (this % i == 0) return false
+    return true
 }
+
+fun mdc(a: Int, b: Int): Int = if (b == 0) a else mdc(b, a % b)
+fun listaPrimosEmRange(range: IntRange): List<Int> = range.filter { it.isPrimo() }
